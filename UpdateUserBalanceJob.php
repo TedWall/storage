1a43f7414d9408bb9e886a0f2b1b1d278e2eafb9
TedWall <40591524+TedWall@users.noreply.github.com>
2025-06-27 14:42:55 +0800

    Update UpdateUserBalanceJob.php

--- UpdateUserBalanceJob.php
+++ UpdateUserBalanceJob.php
@@ -7,17 +7,22 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 
-class UpdateUserBalanceJob implements ShouldQueue
+class UpdateUserBalanceJob implements ShouldQueue,ShouldBeUnique
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     public int $userId;
     public float $amount;
-
+    
     public function __construct(int $userId, float $amount)
     {
         $this->userId = $userId;
         $this->amount = $amount;
+    }
+
+    public function uniqueId(): string
+    {
+        return 'user-balance-' . $this->userId;
     }
 
     public function handle(): void
