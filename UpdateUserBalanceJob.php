519e7a016bc0ad263966dcb23fc53ecf41aa3557
TedWall <40591524+TedWall@users.noreply.github.com>
2025-06-27 11:09:29 +0800

    Create UpdateUserBalanceJob.php

---
+++ UpdateUserBalanceJob.php
@@ -0,0 +1,32 @@
+namespace App\Jobs;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class UpdateUserBalanceJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public int $userId;
+    public float $amount;
+
+    public function __construct(int $userId, float $amount)
+    {
+        $this->userId = $userId;
+        $this->amount = $amount;
+    }
+
+    public function handle(): void
+    {
+        $user = User::findOrFail($this->userId);
+
+        \DB::transaction(function () use ($user) {
+            $user->balance += $this->amount;
+            $user->save();
+        });
+    }
+}
